1461dd527b964697b5f32680e18af8c5efa5dea2
markwh <mark.hagemann@gmail.com>
2015-11-03 18:16:16 -0500

    Fixed arguments to install.packages

--- starterScript.R
+++ starterScript.R
@@ -1,17 +1,17 @@
 # load dependencies
 
 if(!require(devtools)) {
-  install.packages(devtools)
+  install.packages("devtools")
   library(devtools)
 }
 
 if(!require(dplyr)) {
-  install.packages(dplyr)
+  install.packages("dplyr")
   library(dplyr)
 }
 
 if(!require(ggplot2)) {
-  install.packages(ggplot2)
+  install.packages("ggplot2")
   library(ggplot2)
 }
   
